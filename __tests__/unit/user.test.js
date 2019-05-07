a3953fb2c3bc435bcdc8e74c7fb39ffb25d6f037
Beatriz Felix <azz.felix7@gmail.com>
2019-05-07 17:30:46 -0300

    tests for urser

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -7,26 +7,89 @@ const App = require('../../src/App')
 const Trucate = require('../utils/Trucate')
 const Populate = require('../utils/Populate')
 
+const User = require('../../src/Models/UserModel')
+
 describe('User', () => {
+  afterAll(() => setTimeout(() => process.exit(), 1000))
+
   afterEach(async () => {
     await Trucate.users()
   })
 
-  it('should be return a array of users', async () => {
-    await Populate.users(5)
+  it('should be created a user', async done => {
+    const user = await Factory.create('User', {
+      fullName: 'Doe'
+    })
+
+    expect(user).toHaveProperty('_id')
+    expect(user).toHaveProperty('fullName', 'Doe')
+
+    done()
+  })
+
+  it('should encrypt user password', async done => {
+    const user = await Factory.create('User', {
+      password: 'mypass123'
+    })
+
+    const compareHash = await bcrypt.compare('mypass123', user.password)
+
+    expect(compareHash).toBe(true)
+
+    done()
+  })
+
+  it('should be return a array of users', async done => {
+    await Populate.users(3)
 
     const response = await request(App).get('/api/users')
 
     expect(response.status).toBe(200)
+
+    done()
   })
 
-  it('should encrypt user password', async () => {
-    const user = await Factory.create('User', {
-      password: 'mypass'
+  it('should be return one user', async done => {
+    const user = await Populate.user()
+
+    const response = await request(App).get(`/api/users/${user._id}`)
+
+    expect(response.status).toBe(200)
+
+    done()
+  })
+
+  it('should be delete a user', async done => {
+    const user = await Populate.user()
+
+    let response = await request(App).delete(`/api/users/${user._id}`)
+
+    expect(response.status).toBe(200)
+
+    response = await request(App).get(`/api/users/${user._id}`)
+
+    expect(response.status).toBe(400)
+
+    done()
+  })
+
+  it('should be update a user', async done => {
+    let user = await Factory.create('User', {
+      fullName: 'Doe'
     })
 
-    const compareHash = await bcrypt.compare('mypass', user.password)
+    let response = await request(App)
+      .put(`/api/users/${user._id}`)
+      .send({
+        fullName: 'Foo'
+      })
 
-    expect(compareHash).toBe(true)
+    expect(response.status).toBe(200)
+
+    user = await User.findById(user._id)
+
+    expect(user).toHaveProperty('fullName', 'Foo')
+
+    done()
   })
 })
--- __tests__/utils/Populate.js
+++ __tests__/utils/Populate.js
@@ -2,14 +2,30 @@ const faker = require('faker')
 const User = require('../../src/Models/UserModel')
 
 class Populate {
-  users (quantity) {
+  async user (fullName, email, password) {
+    const user = await User.create({
+      fullName: fullName || faker.name.findName(),
+      email: email || faker.internet.email(),
+      password: password || faker.internet.password()
+    })
+
+    return user
+  }
+
+  async users (quantity) {
+    let users = []
+
     for (let i = 0; i < quantity; i++) {
-      User.create({
+      let user = await User.create({
         fullName: faker.name.findName(),
         email: faker.internet.email(),
         password: faker.internet.password()
       })
+
+      users = [...users, user]
     }
+
+    return users
   }
 }
 module.exports = new Populate()
